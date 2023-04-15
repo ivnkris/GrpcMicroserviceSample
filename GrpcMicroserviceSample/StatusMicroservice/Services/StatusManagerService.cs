24909a10f037cd57a04180aba93825adb8a37f1e
Kris Ivan <krisztian.ivan@gmail.com>
2023-04-15 13:54:16 +0100

    added gRPC server

---
+++ GrpcMicroserviceSample/StatusMicroservice/Services/StatusManagerService.cs
@@ -0,0 +1,43 @@
+﻿using Grpc.Core;
+using Status;
+
+namespace StatusMicroservice.Services
+{
+    public class StatusManagerService : StatusManager.StatusManagerBase
+    {
+        private readonly IStateStore stateStore;
+        public StatusManagerService(IStateStore stateStore)
+        {
+            this.stateStore = stateStore;
+        }
+
+        public override async Task GetAllStatuses(ClientStatusesRequest request, IServerStreamWriter<ClientStatusResponse> responseStream, ServerCallContext context)
+        {
+            foreach (var record in stateStore.GetAllStatuses())
+            {
+                await responseStream.WriteAsync(new ClientStatusResponse
+                {
+                    ClientName = record.clientName,
+                    Status = (Status.ClientStatus)record.clientStatus
+                });
+            }
+        }
+
+        public override Task<ClientStatusResponse> GetClientStatus(ClientStatusRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new ClientStatusResponse
+            {
+                ClientName = request.ClientName,
+                Status = (Status.ClientStatus)stateStore.GetStatus(request.ClientName),
+            });
+        }
+
+        public override Task<ClientStatusUpdateResponse> UpdateClientStatus(ClientStatusUpdateRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new ClientStatusUpdateResponse
+            {
+                Success = stateStore.UpdateStatus(request.ClientName, (ClientStatus)request.Status) 
+            });
+        }
+    }
+}
---
+++ GrpcMicroserviceSample/StatusMicroservice/StateStore.cs
@@ -0,0 +1,47 @@
+﻿namespace StatusMicroservice
+{
+    internal class StateStore : IStateStore
+    {
+        private Dictionary<string, ClientStatus> statuses;
+        public StateStore()
+        {
+            statuses = new Dictionary<string, ClientStatus>();
+        }
+
+        public IEnumerable<(string clientName, ClientStatus clientStatus)> GetAllStatuses()
+        {
+            var returnedStatuses = new List<(string clientName, ClientStatus clientStatus)>();
+
+            foreach (var record in statuses)
+            {
+                returnedStatuses.Add((record.Key, record.Value));
+            }
+
+            return returnedStatuses;
+        }
+
+        public ClientStatus GetStatus(string clientName)
+        {
+            if (!statuses.ContainsKey(clientName))
+            {
+                return ClientStatus.OFFLINE;
+            }
+
+            return statuses[clientName];
+        }
+
+        public bool UpdateStatus(string clientName, ClientStatus status)
+        {
+            statuses[clientName] = status;
+
+            return true;
+        }
+    }
+
+    public interface IStateStore
+    {
+        IEnumerable<(string clientName, ClientStatus clientStatus)> GetAllStatuses();
+        ClientStatus GetStatus(string clientName);
+        bool UpdateStatus(string clientName, ClientStatus status);
+    }
+}
