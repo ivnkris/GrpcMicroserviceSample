2a61a1ccac1c173d48c568b4742394852cf8ef37
Kris Ivan <krisztian.ivan@gmail.com>
2023-04-17 16:05:01 +0100

    added gRPC client

---
+++ GrpcMicroserviceSample/ApiGateway/Program.cs
@@ -0,0 +1,32 @@
+using ApiGateway;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddSingleton<IGrpcStatusClient>(p => new GrpcStatusClient("https://localhost:7207"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
